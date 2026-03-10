4315250ee4ec5f9d1f48fc4202e7bbca8336e745
lologaby <agbm97@me.com>
2026-03-10 18:44:06 -0400

    Configure base URL for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://lologaby.github.io',
+  base: '/Dot0x',
   vite: {
     plugins: [tailwindcss()]
   }
